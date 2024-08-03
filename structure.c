94156cc6969dbf185cc6eb6f3f0e42c6d03eb8e6
Anjana-lad <iantanjanalad@gmail.com>
2024-08-03 14:14:46 +0530

    add

---
+++ structure.c
@@ -0,0 +1,22 @@
+// basic structure of c program :-
+
+// 1. documentation section.
+// 2. link section .
+// 3. global declaration section
+// 4. void main(){
+//      // body of the program;
+//     return 0;
+// }
+// 5. user defined function section.
+
+
+// program to print hello world.
+#include<stdio.h>
+void main(){
+    printf("Hello world");
+
+} 
+
+// two ways to create constant in the program
+// 1. using const keyword.
+// 2. using #define variable value
